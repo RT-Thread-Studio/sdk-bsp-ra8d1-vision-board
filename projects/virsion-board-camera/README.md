a9fb6875fb1a977e20cd31d00006da3bc2a1d97f
Rbb666 <zhangbingru@rt-thread.com>
2024-04-06 16:49:17 +0800

    [demo][add]Add camera display project.

---
+++ projects/virsion-board-camera/README.md
@@ -0,0 +1,66 @@
+# virsion-board-camera 示例说明
+
+## 简介
+
+本例程主要功能是摄像头捕获示例，开发板会上电将摄像头捕获的数据显示在LCD屏幕上。
+
+## 硬件说明
+
+* Vision-Board 开发板
+* [屏幕拓展板/套餐](https://item.taobao.com/item.htm?_u=t2dmg8j26111&id=765743204571)
+
+![](docs/picture/shc.png)
+
+MIPI 接口引脚定义如上图所示，需要将屏幕拓展板通过FPC反接排线插入 Vision-Board 的正面MIPI接口中，接线方式见下图：
+
+![](docs/picture/lcd-fpc.png)
+
+## 运行
+
+### 编译&下载
+
+#### MDK 方式
+
+1、双击 `mklinks.bat` 文件，执行脚本后会生成 `rt-thread`、`libraries` 两个文件夹：
+
+![](docs/picture/mklinks.png)
+
+2、编译固件
+
+双击 **project.uvprojx** 文件打开MDK工程
+
+![](docs/picture/uvprojx.png)
+
+点击下图按钮进行项目全编译：
+
+![](docs/picture/build.png)
+
+3、烧录固件
+
+将开发板的 Dap-Link USB 口与 PC 机连接，然后将固件下载至开发板。
+
+![](docs/picture/download.png)
+
+## 运行效果
+
+* 打开 Dap-Link 虚拟出的串口终端，波特率为115200，代码会自动检测插入的摄像头并进行初始化，然后开始捕获RGB数据。
+
+![](docs/picture/shell.png)
+
+* 此时幕拓展板上，会显示摄像头捕获到的画面
+
+## QA
+
+Q：遇到在使用 Dap-Link 时找不到目标芯片的情况并且无法下载：
+
+![](docs/picture/download1.png)
+
+![](docs/picture/download2.png)
+
+A1：方法一：**由于 JTAG/SWD 在芯片出厂时首次使用是加锁的**。请按住开发板的RST按键（正面右一）不松手。再次点击MDK下载按钮后，快速松开RST按键即可。后续无需重复以上操作。
+
+A2：方法二：使用  [Renesas Flash Programmer](https://www.renesas.com/us/en/software-tool/renesas-flash-programmer-programming-gui#documents) 烧录工具进行第一次烧录，操作方法如下：
+
+![](docs/picture/boot1.png)
+
+![](docs/picture/boot2.png)
---
+++ projects/virsion-board-camera/ra_gen/hal_data.c
@@ -0,0 +1,349 @@
+/* generated HAL source file - do not edit */
+#include "hal_data.h"
+
+gpt_instance_ctrl_t g_timer6_ctrl;
+#if 0
+const gpt_extended_pwm_cfg_t g_timer6_pwm_extend =
+{
+    .trough_ipl          = (BSP_IRQ_DISABLED),
+#if defined(VECTOR_NUMBER_GPT6_COUNTER_UNDERFLOW)
+    .trough_irq          = VECTOR_NUMBER_GPT6_COUNTER_UNDERFLOW,
+#else
+    .trough_irq          = FSP_INVALID_VECTOR,
+#endif
+    .poeg_link           = GPT_POEG_LINK_POEG0,
+    .output_disable      = (gpt_output_disable_t) ( GPT_OUTPUT_DISABLE_NONE),
+    .adc_trigger         = (gpt_adc_trigger_t) ( GPT_ADC_TRIGGER_NONE),
+    .dead_time_count_up  = 0,
+    .dead_time_count_down = 0,
+    .adc_a_compare_match = 0,
+    .adc_b_compare_match = 0,
+    .interrupt_skip_source = GPT_INTERRUPT_SKIP_SOURCE_NONE,
+    .interrupt_skip_count  = GPT_INTERRUPT_SKIP_COUNT_0,
+    .interrupt_skip_adc    = GPT_INTERRUPT_SKIP_ADC_NONE,
+    .gtioca_disable_setting = GPT_GTIOC_DISABLE_PROHIBITED,
+    .gtiocb_disable_setting = GPT_GTIOC_DISABLE_PROHIBITED,
+};
+#endif
+const gpt_extended_cfg_t g_timer6_extend =
+{
+    .gtioca = { .output_enabled = true,
+                .stop_level     = GPT_PIN_LEVEL_LOW
+              },
+    .gtiocb = { .output_enabled = false,
+                .stop_level     = GPT_PIN_LEVEL_LOW
+              },
+    .start_source        = (gpt_source_t) ( GPT_SOURCE_NONE),
+    .stop_source         = (gpt_source_t) ( GPT_SOURCE_NONE),
+    .clear_source        = (gpt_source_t) ( GPT_SOURCE_NONE),
+    .count_up_source     = (gpt_source_t) ( GPT_SOURCE_NONE),
+    .count_down_source   = (gpt_source_t) ( GPT_SOURCE_NONE),
+    .capture_a_source    = (gpt_source_t) ( GPT_SOURCE_NONE),
+    .capture_b_source    = (gpt_source_t) ( GPT_SOURCE_NONE),
+    .capture_a_ipl       = (BSP_IRQ_DISABLED),
+    .capture_b_ipl       = (BSP_IRQ_DISABLED),
+#if defined(VECTOR_NUMBER_GPT6_CAPTURE_COMPARE_A)
+    .capture_a_irq       = VECTOR_NUMBER_GPT6_CAPTURE_COMPARE_A,
+#else
+    .capture_a_irq       = FSP_INVALID_VECTOR,
+#endif
+#if defined(VECTOR_NUMBER_GPT6_CAPTURE_COMPARE_B)
+    .capture_b_irq       = VECTOR_NUMBER_GPT6_CAPTURE_COMPARE_B,
+#else
+    .capture_b_irq       = FSP_INVALID_VECTOR,
+#endif
+    .capture_filter_gtioca       = GPT_CAPTURE_FILTER_NONE,
+    .capture_filter_gtiocb       = GPT_CAPTURE_FILTER_NONE,
+#if 0
+    .p_pwm_cfg                   = &g_timer6_pwm_extend,
+#else
+    .p_pwm_cfg                   = NULL,
+#endif
+#if 0
+    .gtior_setting.gtior_b.gtioa  = (0U << 4U) | (0U << 2U) | (0U << 0U),
+    .gtior_setting.gtior_b.oadflt = (uint32_t) GPT_PIN_LEVEL_LOW,
+    .gtior_setting.gtior_b.oahld  = 0U,
+    .gtior_setting.gtior_b.oae    = (uint32_t) true,
+    .gtior_setting.gtior_b.oadf   = (uint32_t) GPT_GTIOC_DISABLE_PROHIBITED,
+    .gtior_setting.gtior_b.nfaen  = ((uint32_t) GPT_CAPTURE_FILTER_NONE & 1U),
+    .gtior_setting.gtior_b.nfcsa  = ((uint32_t) GPT_CAPTURE_FILTER_NONE >> 1U),
+    .gtior_setting.gtior_b.gtiob  = (0U << 4U) | (0U << 2U) | (0U << 0U),
+    .gtior_setting.gtior_b.obdflt = (uint32_t) GPT_PIN_LEVEL_LOW,
+    .gtior_setting.gtior_b.obhld  = 0U,
+    .gtior_setting.gtior_b.obe    = (uint32_t) false,
+    .gtior_setting.gtior_b.obdf   = (uint32_t) GPT_GTIOC_DISABLE_PROHIBITED,
+    .gtior_setting.gtior_b.nfben  = ((uint32_t) GPT_CAPTURE_FILTER_NONE & 1U),
+    .gtior_setting.gtior_b.nfcsb  = ((uint32_t) GPT_CAPTURE_FILTER_NONE >> 1U),
+#else
+    .gtior_setting.gtior = 0U,
+#endif
+};
+
+const timer_cfg_t g_timer6_cfg =
+{
+    .mode                = TIMER_MODE_PERIODIC,
+    /* Actual period: 0.0005461333333333333 seconds. Actual duty: 50%. */ .period_counts = (uint32_t) 0x10000, .duty_cycle_counts = 0x8000, .source_div = (timer_source_div_t)0,
+    .channel             = 6,
+    .p_callback          = NULL,
+    /** If NULL then do not add & */
+#if defined(NULL)
+    .p_context           = NULL,
+#else
+    .p_context           = &NULL,
+#endif
+    .p_extend            = &g_timer6_extend,
+    .cycle_end_ipl       = (BSP_IRQ_DISABLED),
+#if defined(VECTOR_NUMBER_GPT6_COUNTER_OVERFLOW)
+    .cycle_end_irq       = VECTOR_NUMBER_GPT6_COUNTER_OVERFLOW,
+#else
+    .cycle_end_irq       = FSP_INVALID_VECTOR,
+#endif
+};
+/* Instance structure to use this module. */
+const timer_instance_t g_timer6 =
+{
+    .p_ctrl        = &g_timer6_ctrl,
+    .p_cfg         = &g_timer6_cfg,
+    .p_api         = &g_timer_on_gpt
+};
+ceu_instance_ctrl_t g_ceu_qvga_ctrl;
+            const ceu_extended_cfg_t g_ceu_qvga_extended_cfg =
+            {
+                .capture_format       = CEU_CAPTURE_FORMAT_DATA_SYNCHRONOUS,
+                .data_bus_width       = CEU_DATA_BUS_SIZE_8_BIT,
+                .edge_info.dsel       = 0,
+                .edge_info.hdsel      = 0,
+                .edge_info.vdsel      = 0,
+                .hsync_polarity       = CEU_HSYNC_POLARITY_HIGH,
+                .vsync_polarity       = CEU_VSYNC_POLARITY_HIGH,
+                .byte_swapping        = {
+                                        .swap_8bit_units  = (0x1 | 0x2 | 0x4 |  0x0) >> 0x00 & 0x01,
+                                        .swap_16bit_units = (0x1 | 0x2 | 0x4 |  0x0) >> 0x01 & 0x01,
+                                        .swap_32bit_units = (0x1 | 0x2 | 0x4 |  0x0) >> 0x02 & 0x01,
+                                        },
+                .burst_mode           = CEU_BURST_TRANSFER_MODE_X8,
+                .image_area_size      = 320 * 240 * 2,
+                .interrupts_enabled   = 0 | \
+                                        R_CEU_CEIER_CPEIE_Msk | \
+                                        0 | \
+                                        R_CEU_CEIER_VDIE_Msk | \
+                                        R_CEU_CEIER_CDTOFIE_Msk | \
+                                        0 | \
+                                        0 | \
+                                        R_CEU_CEIER_VBPIE_Msk | \
+                                        R_CEU_CEIER_NHDIE_Msk | \
+                                        R_CEU_CEIER_NVDIE_Msk,
+                .ceu_ipl              = (10),
+                .ceu_irq              = VECTOR_NUMBER_CEU_CEUI,
+            };
+
+            const capture_cfg_t g_ceu_qvga_cfg =
+            {
+                .x_capture_pixels      = 320,
+                .y_capture_pixels      = 240,
+                .x_capture_start_pixel = 0,
+                .y_capture_start_pixel = 0,
+                .bytes_per_pixel       = 2,
+                .p_callback            = g_ceu_callback,
+                .p_context             = NULL,
+                .p_extend              = &g_ceu_qvga_extended_cfg,
+            };
+
+            const capture_instance_t g_ceu_qvga =
+            {
+                .p_ctrl = &g_ceu_qvga_ctrl,
+                .p_cfg =  &g_ceu_qvga_cfg,
+                .p_api =  &g_ceu_on_capture,
+            };
+gpt_instance_ctrl_t g_timer7_ctrl;
+#if 0
+const gpt_extended_pwm_cfg_t g_timer7_pwm_extend =
+{
+    .trough_ipl          = (BSP_IRQ_DISABLED),
+#if defined(VECTOR_NUMBER_GPT7_COUNTER_UNDERFLOW)
+    .trough_irq          = VECTOR_NUMBER_GPT7_COUNTER_UNDERFLOW,
+#else
+    .trough_irq          = FSP_INVALID_VECTOR,
+#endif
+    .poeg_link           = GPT_POEG_LINK_POEG0,
+    .output_disable      = (gpt_output_disable_t) ( GPT_OUTPUT_DISABLE_NONE),
+    .adc_trigger         = (gpt_adc_trigger_t) ( GPT_ADC_TRIGGER_NONE),
+    .dead_time_count_up  = 0,
+    .dead_time_count_down = 0,
+    .adc_a_compare_match = 0,
+    .adc_b_compare_match = 0,
+    .interrupt_skip_source = GPT_INTERRUPT_SKIP_SOURCE_NONE,
+    .interrupt_skip_count  = GPT_INTERRUPT_SKIP_COUNT_0,
+    .interrupt_skip_adc    = GPT_INTERRUPT_SKIP_ADC_NONE,
+    .gtioca_disable_setting = GPT_GTIOC_DISABLE_PROHIBITED,
+    .gtiocb_disable_setting = GPT_GTIOC_DISABLE_PROHIBITED,
+};
+#endif
+const gpt_extended_cfg_t g_timer7_extend =
+{
+    .gtioca = { .output_enabled = false,
+                .stop_level     = GPT_PIN_LEVEL_LOW
+              },
+    .gtiocb = { .output_enabled = true,
+                .stop_level     = GPT_PIN_LEVEL_LOW
+              },
+    .start_source        = (gpt_source_t) ( GPT_SOURCE_NONE),
+    .stop_source         = (gpt_source_t) ( GPT_SOURCE_NONE),
+    .clear_source        = (gpt_source_t) ( GPT_SOURCE_NONE),
+    .count_up_source     = (gpt_source_t) ( GPT_SOURCE_NONE),
+    .count_down_source   = (gpt_source_t) ( GPT_SOURCE_NONE),
+    .capture_a_source    = (gpt_source_t) ( GPT_SOURCE_NONE),
+    .capture_b_source    = (gpt_source_t) ( GPT_SOURCE_NONE),
+    .capture_a_ipl       = (BSP_IRQ_DISABLED),
+    .capture_b_ipl       = (BSP_IRQ_DISABLED),
+#if defined(VECTOR_NUMBER_GPT7_CAPTURE_COMPARE_A)
+    .capture_a_irq       = VECTOR_NUMBER_GPT7_CAPTURE_COMPARE_A,
+#else
+    .capture_a_irq       = FSP_INVALID_VECTOR,
+#endif
+#if defined(VECTOR_NUMBER_GPT7_CAPTURE_COMPARE_B)
+    .capture_b_irq       = VECTOR_NUMBER_GPT7_CAPTURE_COMPARE_B,
+#else
+    .capture_b_irq       = FSP_INVALID_VECTOR,
+#endif
+    .capture_filter_gtioca       = GPT_CAPTURE_FILTER_NONE,
+    .capture_filter_gtiocb       = GPT_CAPTURE_FILTER_NONE,
+#if 0
+    .p_pwm_cfg                   = &g_timer7_pwm_extend,
+#else
+    .p_pwm_cfg                   = NULL,
+#endif
+#if 0
+    .gtior_setting.gtior_b.gtioa  = (0U << 4U) | (0U << 2U) | (0U << 0U),
+    .gtior_setting.gtior_b.oadflt = (uint32_t) GPT_PIN_LEVEL_LOW,
+    .gtior_setting.gtior_b.oahld  = 0U,
+    .gtior_setting.gtior_b.oae    = (uint32_t) false,
+    .gtior_setting.gtior_b.oadf   = (uint32_t) GPT_GTIOC_DISABLE_PROHIBITED,
+    .gtior_setting.gtior_b.nfaen  = ((uint32_t) GPT_CAPTURE_FILTER_NONE & 1U),
+    .gtior_setting.gtior_b.nfcsa  = ((uint32_t) GPT_CAPTURE_FILTER_NONE >> 1U),
+    .gtior_setting.gtior_b.gtiob  = (0U << 4U) | (0U << 2U) | (0U << 0U),
+    .gtior_setting.gtior_b.obdflt = (uint32_t) GPT_PIN_LEVEL_LOW,
+    .gtior_setting.gtior_b.obhld  = 0U,
+    .gtior_setting.gtior_b.obe    = (uint32_t) true,
+    .gtior_setting.gtior_b.obdf   = (uint32_t) GPT_GTIOC_DISABLE_PROHIBITED,
+    .gtior_setting.gtior_b.nfben  = ((uint32_t) GPT_CAPTURE_FILTER_NONE & 1U),
+    .gtior_setting.gtior_b.nfcsb  = ((uint32_t) GPT_CAPTURE_FILTER_NONE >> 1U),
+#else
+    .gtior_setting.gtior = 0U,
+#endif
+};
+
+const timer_cfg_t g_timer7_cfg =
+{
+    .mode                = TIMER_MODE_PWM,
+    /* Actual period: 4.166666666666667e-8 seconds. Actual duty: 40%. */ .period_counts = (uint32_t) 0x5, .duty_cycle_counts = 0x2, .source_div = (timer_source_div_t)0,
+    .channel             = 7,
+    .p_callback          = NULL,
+    /** If NULL then do not add & */
+#if defined(NULL)
+    .p_context           = NULL,
+#else
+    .p_context           = &NULL,
+#endif
+    .p_extend            = &g_timer7_extend,
+    .cycle_end_ipl       = (BSP_IRQ_DISABLED),
+#if defined(VECTOR_NUMBER_GPT7_COUNTER_OVERFLOW)
+    .cycle_end_irq       = VECTOR_NUMBER_GPT7_COUNTER_OVERFLOW,
+#else
+    .cycle_end_irq       = FSP_INVALID_VECTOR,
+#endif
+};
+/* Instance structure to use this module. */
+const timer_instance_t g_timer7 =
+{
+    .p_ctrl        = &g_timer7_ctrl,
+    .p_cfg         = &g_timer7_cfg,
+    .p_api         = &g_timer_on_gpt
+};
+sci_b_uart_instance_ctrl_t     g_uart9_ctrl;
+
+            sci_b_baud_setting_t               g_uart9_baud_setting =
+            {
+                /* Baud rate calculated with 0.160% error. */ .baudrate_bits_b.abcse = 0, .baudrate_bits_b.abcs = 0, .baudrate_bits_b.bgdm = 1, .baudrate_bits_b.cks = 0, .baudrate_bits_b.brr = 64, .baudrate_bits_b.mddr = (uint8_t) 256, .baudrate_bits_b.brme = false
+            };
+
+            /** UART extended configuration for UARTonSCI HAL driver */
+            const sci_b_uart_extended_cfg_t g_uart9_cfg_extend =
+            {
+                .clock                = SCI_B_UART_CLOCK_INT,
+                .rx_edge_start          = SCI_B_UART_START_BIT_FALLING_EDGE,
+                .noise_cancel         = SCI_B_UART_NOISE_CANCELLATION_DISABLE,
+                .rx_fifo_trigger        = SCI_B_UART_RX_FIFO_TRIGGER_MAX,
+                .p_baud_setting         = &g_uart9_baud_setting,
+                .flow_control           = SCI_B_UART_FLOW_CONTROL_RTS,
+                #if 0xFF != 0xFF
+                .flow_control_pin       = BSP_IO_PORT_FF_PIN_0xFF,
+                #else
+                .flow_control_pin       = (bsp_io_port_pin_t) UINT16_MAX,
+                #endif
+                .rs485_setting          = {
+                    .enable = SCI_B_UART_RS485_DISABLE,
+                    .polarity = SCI_B_UART_RS485_DE_POLARITY_HIGH,
+                    .assertion_time = 1,
+                    .negation_time = 1,
+                }
+            };
+
+            /** UART interface configuration */
+            const uart_cfg_t g_uart9_cfg =
+            {
+                .channel             = 9,
+                .data_bits           = UART_DATA_BITS_8,
+                .parity              = UART_PARITY_OFF,
+                .stop_bits           = UART_STOP_BITS_1,
+                .p_callback          = user_uart9_callback,
+                .p_context           = NULL,
+                .p_extend            = &g_uart9_cfg_extend,
+#define RA_NOT_DEFINED (1)
+#if (RA_NOT_DEFINED == RA_NOT_DEFINED)
+                .p_transfer_tx       = NULL,
+#else
+                .p_transfer_tx       = &RA_NOT_DEFINED,
+#endif
+#if (RA_NOT_DEFINED == RA_NOT_DEFINED)
+                .p_transfer_rx       = NULL,
+#else
+                .p_transfer_rx       = &RA_NOT_DEFINED,
+#endif
+#undef RA_NOT_DEFINED
+                .rxi_ipl             = (12),
+                .txi_ipl             = (12),
+                .tei_ipl             = (12),
+                .eri_ipl             = (12),
+#if defined(VECTOR_NUMBER_SCI9_RXI)
+                .rxi_irq             = VECTOR_NUMBER_SCI9_RXI,
+#else
+                .rxi_irq             = FSP_INVALID_VECTOR,
+#endif
+#if defined(VECTOR_NUMBER_SCI9_TXI)
+                .txi_irq             = VECTOR_NUMBER_SCI9_TXI,
+#else
+                .txi_irq             = FSP_INVALID_VECTOR,
+#endif
+#if defined(VECTOR_NUMBER_SCI9_TEI)
+                .tei_irq             = VECTOR_NUMBER_SCI9_TEI,
+#else
+                .tei_irq             = FSP_INVALID_VECTOR,
+#endif
+#if defined(VECTOR_NUMBER_SCI9_ERI)
+                .eri_irq             = VECTOR_NUMBER_SCI9_ERI,
+#else
+                .eri_irq             = FSP_INVALID_VECTOR,
+#endif
+            };
+
+/* Instance structure to use this module. */
+const uart_instance_t g_uart9 =
+{
+    .p_ctrl        = &g_uart9_ctrl,
+    .p_cfg         = &g_uart9_cfg,
+    .p_api         = &g_uart_on_sci_b
+};
+void g_hal_init(void) {
+g_common_init();
+}
